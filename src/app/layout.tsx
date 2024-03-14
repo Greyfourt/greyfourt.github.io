54b10d0b7acfb5e9dedebb2742bbb740fc39dd2d
greyfourt <nazliozcubukcuoglu@gmail.com>
2024-03-14 21:09:30 +0100

    matomo fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Matomo />
+      </head>
       <body className={nunito.className}>
         <Menu />
         {children}
