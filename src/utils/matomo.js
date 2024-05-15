5347ff5e75d12dc30485b438600ebacac52dfa5b
greyfourt <nazliozcubukcuoglu@gmail.com>
2024-05-15 13:08:57 +0200

    clean up

--- src/utils/matomo.js
+++ src/utils/matomo.js
@@ -1,32 +1,3 @@
-// import Script from "next/script";
-
-// const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-// const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-
-// export default function Matomo() {
-//     return (
-//         <Script id="matomo" strategy="afterInteractive">
-//             {`
-//                 var _paq = window._paq = window._paq || [];
-//                 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-//                  _paq.push(['trackPageView']);
-//                  _paq.push(['enableLinkTracking']);
-                
-//                  (function() {
-//                     var u="//${MATOMO_URL}/";
-//                     _paq.push(['setTrackerUrl', u+'matomo.php']);
-//                     _paq.push(['setSiteId', '${MATOMO_SITE_ID}']);
-//                     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-//                     g.async=true;
-//                     g.src=u+'matomo.js'; 
-//                     s.parentNode.insertBefore(g,s);
-//              })();
-//              `}
-//         </Script>
-//     )
-// }
-
-
 'use client'
 import { init, push } from '@socialgouv/matomo-next'
 import { usePathname, useSearchParams } from 'next/navigation'
