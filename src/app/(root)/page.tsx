b8ca1091625a9d36a75bf745a986ba62c0a06829
Greyfourt <nazliozcubukcuoglu@gmail.com>
2026-02-24 10:02:26 +0100

    fix and clean up structure for multilang setup

--- src/app/(root)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function RedirectLayout() {
-   redirect('/en')
-  }
-  
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h2>Not Found</h2>
+        <p>Could not find requested resource</p>
+        <a href="/">Return Home</a>
+      </body>
+    </html>
+  );
+}
